74f65254a380efeb003ab6a2577201a52db4b531
Atanas Bozhkov <bozhkov.atanas@gmail.com>
2013-09-28 20:11:35 +0100

    Added a visualisations for the IR sensors

---
+++ vis.py
@@ -0,0 +1,43 @@
+import serial
+import matplotlib.pyplot as plt
+import numpy as np
+
+ser = serial.Serial(0, 9600)
+
+def setup_backend(backend='TkAgg'):
+    import sys
+    del sys.modules['matplotlib.backends']
+    del sys.modules['matplotlib.pyplot']
+    import matplotlib as mpl
+    mpl.use(backend)  # do this before importing pyplot
+    import matplotlib.pyplot as plt
+    return plt
+
+#Returns a list with the 8 elements for the sensor values. 
+def readIR(ser):
+        ser.write("N\n")
+        sensorString = ser.readline()
+        # Drop "\r\n" at the end of string and "n" at beginning
+        return sensorString[:-2].split(",")[1:]  
+
+
+def animate():
+    N = 8
+    rects = plt.bar(range(N), x, align='center')
+
+    while True:
+    	
+    	for i in range(8):
+    		# x = data[i]
+    		for rect, h in zip(rects, readIR(ser)):
+    			if(h.isdigit()==True):
+    				rect.set_height(int(h))
+    		fig.canvas.draw()
+
+# plt.yticks(range(1,1000))
+plt = setup_backend()
+fig = plt.figure()
+plt.axis([0, 7, 0, 1100])
+win = fig.canvas.manager.window
+win.after(10, animate)
+plt.show()
