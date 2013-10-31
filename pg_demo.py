954eb8193e9673b3393484e3432192902f9b7751
Georgi Koshov <koshov@gmail.com>
2013-10-31 18:07:04 +0000

    More Virtualenv

--- pg_demo.py
+++ pg_demo.py
@@ -5,18 +5,19 @@ from pygame.locals import *
 pygame.init()
 
 # set up the window
-windowSurface = pygame.display.set_mode((500, 400), 0, 32)
-pygame.display.set_caption('Hello world!')
+windowSurface = pygame.display.set_mode((900, 900), 0, 32)
+pygame.display.set_caption('Goshko & Nasko rule!')
 
 # set up the colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
+RED = (228, 120, 118)
+ORANGE = (237, 151, 81)
+GREEN = (195, 237, 81)
+BLUE = (32, 118, 186)
 
 # draw the white background onto the surface
-windowSurface.fill(WHITE)
+windowSurface.fill(BLUE)
 
 # draw a green polygon onto the surface
 pygame.draw.polygon(windowSurface, GREEN, ((146, 0), (291, 106), (236, 277), (56, 277), (0, 106)))
@@ -24,7 +25,7 @@ pygame.draw.polygon(windowSurface, GREEN, ((146, 0), (291, 106), (236, 277), (56
 # draw some blue lines onto the surface
 pygame.draw.line(windowSurface, BLUE, (60, 60), (120, 60), 4)
 pygame.draw.line(windowSurface, BLUE, (120, 60), (60, 120))
-pygame.draw.line(windowSurface, BLUE, (60, 120), (120, 120), 4)
+pygame.draw.line(windowSurface, RED, (60, 120), (120, 120), 4)
 
 # draw a blue circle onto the surface
 pygame.draw.circle(windowSurface, BLUE, (300, 50), 20, 0)
@@ -36,7 +37,7 @@ pygame.draw.ellipse(windowSurface, RED, (300, 250, 40, 80), 1)
 pixArray = pygame.PixelArray(windowSurface)
 for x in range(100):
     for y in range(100):
-        pixArray[x+200][y+300] = BLACK
+        pixArray[x+200][y+300] = ORANGE
 del pixArray
 
 
